0392a2642231fc441b712fd0970e213ea795ad3e
bornwarrior <j3patel@gmail.com>
2019-02-05 05:41:18 -0500

    added missing files

--- io/ExerciseTryWithResources/src/com/company/Main.java
+++ io/ExerciseTryWithResources/src/com/company/Main.java
@@ -1,8 +1,92 @@
 package com.company;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        System.out.println(System.getProperty("user.dir"));
+        System.out.println("--------------------------");
+        //doTryCatchFinally();
+         //doTryWithResources();
+        //doTryWithResourcesMulti();
+        doCloseThing();
     }
+
+    public static void doTryCatchFinally() {
+        char[] buff = new char[8];
+        int length;
+        Reader reader = null;
+        System.out.println("doTryCatchFinally");
+
+        try {
+            reader = Helper.openReader("file1.txt");
+            while ((length = reader.read(buff)) >= 0) {
+                System.out.println("\nlength: " + length);
+                for (int i = 0; i < length; i++)
+                    System.out.print(buff[i]);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getClass().getSimpleName() + " - " + e.getMessage());
+        } finally {
+            try {
+                if (reader != null)
+                    reader.close();
+            } catch (IOException e2) {
+                System.out.println(e2.getClass().getSimpleName() + " - " + e2.getMessage());
+            }
+        }
+    }
+
+    public static void doTryWithResources() {
+        char[] buff = new char[8];
+        int length;
+        System.out.println("doTryWithResources");
+
+        try (Reader reader = Helper.openReader("file1.txt")) {
+
+            while ((length = reader.read(buff)) >= 0) {
+                System.out.println("\nlength: " + length);
+                for (int i = 0; i < length; i++)
+                    System.out.print(buff[i]);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getClass().getSimpleName() + " - " + e.getMessage());
+        }
+
+    }
+
+    public static void doTryWithResourcesMulti() {
+        System.out.println("doTryWithResourcesMulti");
+        char[] buff = new char[8];
+        int length;
+        try (Reader reader = Helper.openReader("file1.txt");
+             Writer writer = Helper.openWriter("file2.txt")) {
+
+            while ((length = reader.read(buff)) >= 0) {
+                System.out.println("\nlength: " + length);
+                writer.write(buff,0,length);;
+            }
+        } catch (IOException e) {
+            System.out.println(e.getClass().getSimpleName() + " - " + e.getMessage());
+        }
+
+
+    }
+
+    private static void doCloseThing() {
+        System.out.println("doCloseThing");
+        try (MyAutoCloseable ac = new MyAutoCloseable()) {
+            ac.saySomething();
+        } catch (IOException e) {
+            System.out.println(e.getClass().getSimpleName() + " - " + e.getMessage());
+            for(Throwable t:e.getSuppressed()) {
+                System.out.println("Suppressed: " + t.getMessage());
+            }
+        }
+    }
+
 }
--- io/ExerciseTryWithResources/src/com/company/MyAutoCloseable.java
+++ io/ExerciseTryWithResources/src/com/company/MyAutoCloseable.java
@@ -1,4 +1,18 @@
 package com.company;
 
-public class MyAutoCloseable {
+import java.io.IOException;
+
+public class MyAutoCloseable implements AutoCloseable {
+    @Override
+    public void close() throws IOException {
+        //System.out.println("close");
+        throw new  IOException("Exception from close");
+
+    }
+
+    public void saySomething() throws IOException {
+        throw new  IOException("Exception from saySomething");
+        //System.out.println("Something");
+    }
+
 }
